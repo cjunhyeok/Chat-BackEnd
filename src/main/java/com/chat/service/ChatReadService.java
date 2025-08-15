e467677a1e0a5d59c9d52421c9e303591849e7b9
Choi jun hyoek <chlwnsgur1214@naver.com>
2025-08-16 00:21:45 +0900

    마지막 읽은 채팅 ID 조회 로직 변경 - 채팅이 없으면 빈 데이터를 조회하던 로직에서 데이터가 없을 시 기본값을 설정하는 로직으로 변경

--- src/main/java/com/chat/service/ChatReadService.java
+++ src/main/java/com/chat/service/ChatReadService.java
@@ -1,6 +1,7 @@
 package com.chat.service;
 
 import com.chat.repository.ChatReadRepository;
+import com.chat.repository.MemberRepository;
 import com.chat.service.dtos.LastChatRead;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -16,9 +19,30 @@ import java.util.List;
 public class ChatReadService {
 
     private final ChatReadRepository chatReadRepository;
+    private final MemberRepository memberRepository;
 
     public List<LastChatRead> findMembersChatReadIn(Long chatRoomId) {
-        return chatReadRepository.findLastReadChatsBy(chatRoomId);
+
+        Map<Long, Long> membersLastChatId = getMembersLastChatIdsMapBy(chatRoomId);
+
+        List<Long> memberIdsInChatRoom = memberRepository.findMemberIdsIn(chatRoomId);
+        List<LastChatRead> finalLastReads = memberIdsInChatRoom.stream()
+                .map(memberId -> {
+                    Long lastReadId = membersLastChatId.getOrDefault(memberId, 0L);
+                    return new LastChatRead(memberId, lastReadId);
+                })
+                .collect(Collectors.toList());
+
+        return finalLastReads;
+    }
+
+    private Map<Long, Long> getMembersLastChatIdsMapBy(Long chatRoomId) {
+        // 각 회원별 마지막 읽은 메시지 ID 조회
+        List<LastChatRead> lastReadChats = chatReadRepository.findLastReadChatsBy(chatRoomId);
+        // 1. lastReadChats를 Map<Long, Long> 형태로 변환
+        //    key: memberId, value: lastChatReadId
+        return lastReadChats.stream()
+                .collect(Collectors.toMap(LastChatRead::getMemberId, LastChatRead::getLastChatReadId));
     }
 
     public LastChatRead findLastChatBy(Long memberId, Long chatRoomId) {
